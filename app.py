713f9b8aaa4aaa6db812e92cbfe2c3d523cec3ce
DROID <mnyaanga44@gmail.com>
2024-11-05 19:15:47 +0300

    initial

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #made some change
 @app.route("/")
 def home():
-    return "<h1>Hello, this is a test page for the Flask application, welcome!</h1>"
+    return "<h1>Hello, this is a test page for the Flask application, welcome!  </h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
