f9ec0c7fdf60caee56ff682b1d7a74caad21ccd6
DROID <mnyaanga44@gmail.com>
2024-11-07 13:55:07 +0300

    1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #made some change made to this code. Welcome to page. Some changes again jiji made some changes at home.
 @app.route("/")
 def home():
-    return "<body style='color:red';width:100%;height:100%><h1>Hello, this is a test page for the Flask application, welcome! Thursday.  </h1></body>"
+    return "<body style='color:red';width:100%;height:100%><h1>Hello, this is a test page for the Flask application mahn, welcome! Thursday.  </h1></body>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
