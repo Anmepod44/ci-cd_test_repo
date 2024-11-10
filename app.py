02c503e8c4ee8f52ca636179cd4f93366f92b22a
DROID <mnyaanga44@gmail.com>
2024-11-10 18:35:09 +0300

    juj

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-#made some change made to this code. Welcome to page. Some changes again jiji made some changes at home. Made an update to the code here !
+#made some change made to this code. Welcome to page. Some changes again jiji made some changes at home. Made an update to the code here ! welcome home
 @app.route("/")
 def home():
     return "<body style='color:red';width:100%;height:100%><h1>Hello, welcome back to my dummy page guys.  </h1></body>"
