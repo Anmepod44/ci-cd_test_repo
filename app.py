70ffdd1cf0dc2d19661d22d2f2c4e8d340fe9e60
MARVIN <mnyaranga44@gmail.com>
2024-11-18 12:47:23 +0300

    updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #made some change made to this code. Welcome to page. Some changes again jiji made some changes at home. Made an update to the code here ! welcome home
 @app.route("/")
 def home():
-    return "<body style='color:red';width:100%;height:100%><h1>Hello, welcome back to my dummy page guys. This is an update  </h1></body>"
+    return "<body style='color:red';width:100%;height:100%><h1>Hello, welcome back to the backend dummy page </h1></body>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
